7c5d0d935d352d21538bee27c9230e1d2eb5100d
Rachele Suy <rachelesuy@gmail.com>
2019-09-09 13:57:54 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Sinatra Forms Lab
+  # Sinatra Forms Lab
 
 ## Overview
 
--- app.rb
+++ app.rb
@@ -2,5 +2,16 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get '/newteam' do 
+      erb :team
+    end
+    
+    
+    post '/team' do 
+      @team = params
+    erb :team
+    end
+
+
 
 end
